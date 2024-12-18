74e04cd9042c66d48122e39600eac14015129202
NikKha03 <kolyakhalimendik@gmail.com>
2024-12-18 23:05:43 +0300

    с ts на js

---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,75 @@
+import React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import IconButton from '@mui/material/IconButton';
+import Typography from '@mui/material/Typography';
+import MenuItem from '@mui/material/MenuItem';
+import Menu from '@mui/material/Menu';
+import AccountCircle from '@mui/icons-material/AccountCircle';
+
+export default function Navbar() {
+	const [anchorEl, setAnchorEl] = React.useState(null);
+	const isMenuOpen = Boolean(anchorEl);
+
+	const handleProfileMenuOpen = event => {
+		setAnchorEl(event.currentTarget);
+	};
+
+	const handleMenuClose = () => {
+		setAnchorEl(null);
+	};
+
+	const menuId = 'primary-search-account-menu';
+	const renderMenu = (
+		<Menu
+			anchorEl={anchorEl}
+			anchorOrigin={{
+				vertical: 'top',
+				horizontal: 'right',
+			}}
+			id={menuId}
+			keepMounted
+			transformOrigin={{
+				vertical: 'top',
+				horizontal: 'right',
+			}}
+			open={isMenuOpen}
+			onClose={handleMenuClose}
+		>
+			<MenuItem
+				onClick={() => {
+					location.href = '#';
+				}}
+			>
+				Профиль
+			</MenuItem>
+			<MenuItem
+				onClick={() => {
+					window.location.href = '#';
+				}}
+			>
+				Выйти
+			</MenuItem>
+		</Menu>
+	);
+
+	return (
+		<Box sx={{ flexGrow: 1 }}>
+			<AppBar position='static' sx={{ paddingLeft: 3.5, paddingRight: 3.5, backgroundColor: '#1e1e1e' }}>
+				<Toolbar>
+					<Box component='a' href='#' variant='h4' noWrap sx={{ display: { xs: 'none', md: 'flex' } }}>
+						<img src='../../../icons/logo.png' alt='' loading='lazy' />
+					</Box>
+					<Box sx={{ flexGrow: 1 }} />
+					<Box sx={{ display: { xs: 'none', md: 'flex' } }}>
+						<IconButton size='large' edge='end' aria-label='account of current user' aria-controls={menuId} aria-haspopup='true' onClick={handleProfileMenuOpen} color='inherit'>
+							<AccountCircle />
+						</IconButton>
+					</Box>
+				</Toolbar>
+			</AppBar>
+			{renderMenu}
+		</Box>
+	);
+}
